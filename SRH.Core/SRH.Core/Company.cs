2aa9eeb82c30b5e1bcaebbc09ab853645768e94a
Erwandebenaze <duhildebenaze@intechinfo.fr>
2014-10-30 11:16:15 +0100

    Création de la solution
    Début classe Company

---
+++ SRH.Core/SRH.Core/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRH.Core
+{
+    public class Company
+    {
+        readonly string _name;
+        int _companyLevel;
+
+        public Company( string name )
+        {
+            if( name == null ) throw new ArgumentNullException( "name" );
+            //TODO : Check if the name is already existing
+            _name = name;
+            _companyLevel = 0;
+        }
+    }
+}
