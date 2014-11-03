1181afbe799e95a9d374128dd84e469e9a7bf07b
Titi <tristanux@gmail.com>
2014-11-03 09:23:39 +0100

    Ajout classe RandomNameGenerator

---
+++ SRH.Core/SRH.Core/RandomNameGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRH.Core
+{
+	class RandomNameGenerator
+	{
+
+		class RandomNameGenerator
+	{
+
+		readonly List<string> _firstNames;
+		readonly List<string> _lastNames;
+
+		public RandomNameGenerator ()
+		{
+            _firstNames = new List<string>();
+            _lastNames = new List<string>();
+            foreach( string line in File.ReadLines( Directory.GetCurrentDirectory() + @"\Data\FirstNames.txt" ) )
+            {
+                _firstNames.Add( line );
+            }
+            foreach( string line in File.ReadLines( Directory.GetCurrentDirectory() + @"\Data\LastNames.txt" ) )
+            {
+                _lastNames.Add( line );
+            }
+		}
+
+		public List<string> FirstNames
+		{
+			get { return _firstNames; }
+		}
+		public List<string> LastNames
+		{
+			get { return _lastNames; }
+		}
+
+	}
+}
