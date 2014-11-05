caae6dfb428d2716486e75af8db9f051178adc92
Erwandebenaze <duhildebenaze@intechinfo.fr>
2014-11-05 13:39:11 +0100

    Tests unitaires pour Company et MyCompany prêt et non testés

--- SRH.Core/SRH.Core.Tests/CompanyTests.cs
+++ SRH.Core/SRH.Core.Tests/CompanyTests.cs
@@ -8,13 +8,50 @@ using SRH.Core;
 
 namespace SRH.Core.Tests
 {
-    //[TestFixture]
-    //class CompanyTests
-    //{
-    //    [Test]
-    //    //public void Create_company()
-    //    //{
-    //    //    Company c = new Company("Danone");
-    //    //}
-    //}
+    [TestFixture]
+    class CompanyTests
+    {
+        [Test]
+        public void Create_company()
+        {
+            Company c = new Company();
+            Assert.That( c, Is.Not.Null);
+        }
+
+        [Test]
+        public void Create_mycompany()
+        {
+            MyCompany mc = new MyCompany("Danone");
+            Assert.That( mc, Is.Not.Null );
+        }
+
+        [Test]
+        [ExpectedException( typeof( ArgumentNullException ) )]
+        public void Create_mycompany_with_white_space_throw_argument_null_exeption()
+        {
+            MyCompany mc = new MyCompany( " " );
+        }
+
+        [Test]
+        public void New_company_have_10_max_employees()
+        {
+            MyCompany mc = new MyCompany( "Danone" );
+            Assert.That( mc.MaxEmployees, Is.EqualTo(10) );
+        }
+
+        [Test]
+        public void New_company_have_1_max_project_difficulty()
+        {
+            MyCompany mc = new MyCompany( "Danone" );
+            Assert.That( mc.MaxProjectDifficulty, Is.EqualTo( 1 ) );
+        }
+
+        [Test]
+        public void Our_company_takes_experience_and_upgrade_his_level()
+        {
+            MyCompany mc = new MyCompany( "Danone" );
+            mc.Level.IncreaseXp( 100 );
+            Assert.That( mc.Level.CurrentLevel, Is.EqualTo( 2 ) );
+        }
+    }
 }
